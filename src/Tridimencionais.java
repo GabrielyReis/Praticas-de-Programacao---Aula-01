8caa7a4085968544317812404b4ab063aa9cded3
GabrielyReis <37003990+GabrielyReis@users.noreply.github.com>
2018-03-04 13:24:23 -0300

    Add files via upload

---
+++ src/Tridimencionais.java
@@ -0,0 +1,13 @@
+
+public abstract class Tridimencionais extends Figura {
+	
+
+	public Tridimencionais(int altura, int base, int raio) {
+		super(altura, base, raio);
+		// TODO Auto-generated constructor stub
+	}
+
+    
+	public abstract double volume();
+       
+}
